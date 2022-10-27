b11772566c720963f490ba4996ee10176b013df0
alvrobertdev <robert.alvarado@flexibility.com.ar>
2022-10-26 21:34:48 -0300

    create proyect

---
+++ src/main/java/com/heroes/challenge/robert/ChallengueRobertApplication.java
@@ -0,0 +1,13 @@
+package com.heroes.challenge.robert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengueRobertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengueRobertApplication.class, args);
+	}
+
+}
