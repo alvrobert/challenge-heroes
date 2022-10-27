b24e5529f2acef641504a8b0326f1a73ae92fe61
alvrobertdev <robert.alvarado@flexibility.com.ar>
2022-10-27 17:10:46 -0300

    add unit test

--- src/test/java/com/heroes/challenge/robert/controller/ClientControllerTest.java
+++ src/test/java/com/heroes/challenge/robert/controller/ClientControllerTest.java
@@ -1,52 +1,46 @@
 package com.heroes.challenge.robert.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.heroes.challenge.robert.controller.fixture.ClientResponseDTOFixture;
-import com.heroes.challenge.robert.dtos.ClientResponseDTO;
 import com.heroes.challenge.robert.service.ClientService;
-import com.heroes.challenge.robert.util.ClientResponse;
-import com.heroes.challenge.robert.util.exceptions.ClientException;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ClientController.class)
 class ClientControllerTest {
 
     private static final String SUCCESS_STATUS = "Success";
-
+    @MockBean
+    ClientService clientService;
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    ClientService clientService;
-
-
+    ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
+        objectMapper = new ObjectMapper();
     }
 
     @Test
-    void testGetClientByDocument() throws Exception {
+    void testGetClientByDocumentTest() throws Exception {
         when(clientService.getClientByDocument(any())).thenReturn(ClientResponseDTOFixture.withDefaults());
         mvc.perform(get("/client/client/1").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("OK"))
+                .andExpect(jsonPath("$.data.document").value("123477"));
     }
 }
--- src/test/java/com/heroes/challenge/robert/service/impl/ClientServiceImplTest.java
+++ src/test/java/com/heroes/challenge/robert/service/impl/ClientServiceImplTest.java
@@ -1,4 +1,43 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.heroes.challenge.robert.service.impl;
+
+import com.heroes.challenge.robert.entity.Client;
+import com.heroes.challenge.robert.repositoy.ClientRepository;
+import com.heroes.challenge.robert.service.ClientService;
+import com.heroes.challenge.robert.util.exceptions.ClientException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 class ClientServiceImplTest {
-  
+
+    ClientRepository clientRepository;
+    ClientService clientService;
+
+    @BeforeEach
+    void setUp() {
+        clientRepository = mock(ClientRepository.class);
+        clientService = new ClientServiceImpl(clientRepository);
+    }
+
+    @Test
+    void getClientByDocumentTest() throws ClientException {
+        Client client = Client.builder()
+                .document(BigInteger.valueOf(123))
+                .lastName("Last Name")
+                .name("name")
+                .email("email").build();
+        when(clientRepository.findByDocument(any())).thenReturn(Optional.ofNullable(client));
+        String name = clientService.getClientByDocument(any()).getName();
+        assertEquals("name", name);
+    }
+
 }
